87efadb256143a09e9bfaa9549d029dedd025992
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-10-14 21:02:05 +0200

    write mapped and missed item ids to file

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import re
 file_4_3_8_txt = "input/items-4-3-8.txt"
 file_4_3_8_json = "output/items-4-3-8.json"
 file_4_8_6_json = "input/items-4-8-6.json"
+mapped_item_ids_json = "output/mapped-item-ids.json"
+missed_item_ids_json = "output/missed-item-ids.json"
 
 def convert_input_text_to_json(input_file, output_file):
     data = {}
@@ -30,7 +32,43 @@ def convert_input_text_to_json(input_file, output_file):
 def get_4_8_6_data(file):
     with open(file, "r") as f:
         return json.load(f)
+    
+def map_item_ids(data_4_3_8, data_4_8_6):
+    mapped_item_ids = {}
+    # iterate over 4.3.8 item ids and find matches in 4.8.6 called ID
+    # go in lists of 4.8.6 and find ID there
+    # Ensure both data sources are dictionaries mapping IDs to names
+    # For each key, value in data_4_8_6, handle if value is a list or single item
+    items_list_4_8_6 = []
+    for key, value in data_4_8_6.items():
+        if isinstance(value, list):
+            items_list_4_8_6.extend(value)
+
+    # for item in items_list_4_8_6:
+    #     print(item["ID"])
+
+    for item_id_4_3_8, item_name_4_3_8 in data_4_3_8.items():
+        for item_4_8_6 in items_list_4_8_6:
+            if item_4_8_6["Name"] == item_name_4_3_8:
+                mapped_item_ids[item_id_4_3_8] = item_4_8_6["ID"]
+
+    with open(mapped_item_ids_json, "w") as f:
+        json.dump(mapped_item_ids, f, indent=4)
+    print(f"Mapped {len(mapped_item_ids)} item ids from 4.3.8 to 4.8.6, written to {mapped_item_ids_json}")
+
+    missed_item_ids = {}
+    for key in data_4_3_8.keys():
+        if key not in mapped_item_ids:
+            missed_item_ids[key] = 0
+
+    with open(missed_item_ids_json, "w") as f:
+        json.dump(missed_item_ids, f, indent=4)
+    print(f"missed {len(missed_item_ids)} item ids from 4.3.8 to 4.8.6, written to {missed_item_ids_json}")
+
+    return mapped_item_ids, missed_item_ids
+
 
 if __name__ == "__main__":
     data_4_3_8 = convert_input_text_to_json(file_4_3_8_txt, file_4_3_8_json)
     data_4_8_6 = get_4_8_6_data(file_4_8_6_json)
+    mapped_item_ids, missed_item_ids = map_item_ids(data_4_3_8, data_4_8_6)
