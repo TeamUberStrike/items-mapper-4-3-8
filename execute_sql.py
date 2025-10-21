b47e91796a744b99f4ad3902e36428d3526a9f2e
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-10-21 20:38:05 +0200

    add item id to config and change to function

--- execute_sql.py
+++ execute_sql.py
@@ -1,105 +1,99 @@
 import pyodbc
 
-# 1️⃣ Connect to SQL Server
-conn = pyodbc.connect(
-    "DRIVER={ODBC Driver 17 for SQL Server};"
-    "SERVER=DESKTOP-LNSADFU\MYSECONDSERVER;"          # or 'hostname\\SQLEXPRESS'
-    "DATABASE=Cmune;"
-    "UID=sa;"
-    "PWD=cmune$1;"
-)
+def add_item_to_database(config):
 
-cursor = conn.cursor()
+    # 1️⃣ Connect to SQL Server
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=DESKTOP-LNSADFU\MYSECONDSERVER;"          # or 'hostname\\SQLEXPRESS'
+        "DATABASE=Cmune;"
+        "UID=sa;"
+        "PWD=cmune$1;"
+    )
 
-# 2️⃣ Prepare the INSERT query
-sql = """
-INSERT INTO dbo.Items (
-    Name,
-    Description,
-    CreditsPerDayShop,
-    PointsPerDayShop,
-    TypeId,
-    IsForSale,
-    AmountRemainingInShop,
-    IsFeatured,
-    PurchaseType,
-    PermanentCreditsShop,
-    IsNew,
-    IsPopular,
-    ClassId,
-    PackOneAmount,
-    PackTwoAmount,
-    PackThreeAmount,
-    MaximumOwnableAmount,
-    Enable1Day,
-    Enable7Days,
-    Enable30Days,
-    Enable90Days,
-    MaximumDurationDays,
-    PermanentPointsShop,
-    IsDisable,
-    CustomProperties,
-    IsEnabledInShop,
-    CreditsPerDayUnderground,
-    PermanentCreditsUnderground,
-    IsEnabledInUnderground,
-    AmountRemainingInUnderground,
-    UsageCount
-)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-"""
-config = {}
-config['name'] = ""
-config['description'] = ""
-config['type_id'] = 0
-config['class_id'] = 0
-# config['permanent_points'] = 0
-# config['permanent_credits'] = 0
-# config['points_per_day'] = 0
-# config['credits_per_day'] = 0
+    cursor = conn.cursor()
 
+    # 2️⃣ Prepare the INSERT query
+    sql = """
+    INSERT INTO dbo.Items (
+        ItemId,
+        Name,
+        Description,
+        CreditsPerDayShop,
+        PointsPerDayShop,
+        TypeId,
+        IsForSale,
+        AmountRemainingInShop,
+        IsFeatured,
+        PurchaseType,
+        PermanentCreditsShop,
+        IsNew,
+        IsPopular,
+        ClassId,
+        PackOneAmount,
+        PackTwoAmount,
+        PackThreeAmount,
+        MaximumOwnableAmount,
+        Enable1Day,
+        Enable7Days,
+        Enable30Days,
+        Enable90Days,
+        MaximumDurationDays,
+        PermanentPointsShop,
+        IsDisable,
+        CustomProperties,
+        IsEnabledInShop,
+        CreditsPerDayUnderground,
+        PermanentCreditsUnderground,
+        IsEnabledInUnderground,
+        AmountRemainingInUnderground,
+        UsageCount
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """
 
-# 3️⃣ Define the values to insert
-values = (
-    'Sword of Dawn',                    # Name
-    'A rare sword imbued with light.',  # Description
-    1000,                               # CreditsPerDayShop
-    1000,                               # PointsPerDayShop
-    2,                                  # TypeId
-    False,                              # IsForSale
-    100000,                             # AmountRemainingInShop
-    False,                              # IsFeatured
-    1,                                  # PurchaseType
-    100000,                             # PermanentCreditsShop
-    False,                              # IsNew
-    False,                              # IsPopular
-    3,                                  # ClassId
-    1,                                  # PackOneAmount
-    0,                                  # PackTwoAmount
-    0,                                  # PackThreeAmount
-    1,                                  # MaximumOwnableAmount
-    True,                               # Enable1Day
-    True,                               # Enable7Days
-    True,                               # Enable30Days
-    False,                              # Enable90Days
-    30,                                 # MaximumDurationDays
-    100000,                             # PermanentPointsShop
-    False,                              # IsDisable
-    '',                                 # CustomProperties
-    True,                               # IsEnabledInShop
-    1000,                               # CreditsPerDayUnderground
-    100000,                             # PermanentCreditsUnderground
-    False,                              # IsEnabledInUnderground
-    100000,                             # AmountRemainingInUnderground
-    0                                   # UsageCount
-)
+    # 3️⃣ Define the values to insert
+    values = (
+        config['item_id'],                  # ItemId
+        config['name'],                     # Name
+        config['description'],              # Description
+        1000,                               # CreditsPerDayShop
+        1000,                               # PointsPerDayShop
+        config['type_id'],                  # TypeId
+        False,                              # IsForSale
+        100000,                             # AmountRemainingInShop
+        False,                              # IsFeatured
+        1,                                  # PurchaseType
+        100000,                             # PermanentCreditsShop
+        False,                              # IsNew
+        False,                              # IsPopular
+        config['class_id'],                 # ClassId
+        1,                                  # PackOneAmount
+        0,                                  # PackTwoAmount
+        0,                                  # PackThreeAmount
+        1,                                  # MaximumOwnableAmount
+        True,                               # Enable1Day
+        True,                               # Enable7Days
+        True,                               # Enable30Days
+        False,                              # Enable90Days
+        30,                                 # MaximumDurationDays
+        100000,                             # PermanentPointsShop
+        False,                              # IsDisable
+        '',                                 # CustomProperties
+        True,                               # IsEnabledInShop
+        1000,                               # CreditsPerDayUnderground
+        100000,                             # PermanentCreditsUnderground
+        False,                              # IsEnabledInUnderground
+        100000,                             # AmountRemainingInUnderground
+        0                                   # UsageCount
+    )
 
-# 4️⃣ Execute and commit
-cursor.execute(sql, values)
-conn.commit()
+    # 4️⃣ Execute and commit
+    cursor.execute(sql, values)
+    conn.commit()
 
-print("✅ Item inserted successfully!")
+    print("✅ Item inserted successfully!")
 
-# 5️⃣ Clean up
-cursor.close()
-conn.close()
+    # 5️⃣ Clean up
+    cursor.close()
+    conn.close()
