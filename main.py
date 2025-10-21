d905d36ff2deb72f37df265f617fe18ae8993615
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-10-21 21:15:11 +0200

    create config file for sql

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ manual_item_mappings_json = "input/manual-item-mappings.json"
 mapped_item_ids_json = "output/mapped-item-ids.json"
 missed_item_ids_json = "output/missed-item-ids.json"
 total_mapped_item_ids_json = "output/total-mapped-item-ids.json"
+sql_config_list_json = "output/sql-config-list.json"
 
 def convert_input_text_to_json(input_file, output_file):
     data = {}
@@ -35,13 +36,15 @@ def get_4_8_6_data(file):
     with open(file, "r") as f:
         return json.load(f)
     
-def map_item_ids(data_4_3_8, data_4_8_6):
-    mapped_item_ids = {}
-
-    items_list_4_8_6 = []
+def get_items_list_4_8_6(data_4_8_6):
+    items_list = []
     for key, value in data_4_8_6.items():
         if isinstance(value, list):
-            items_list_4_8_6.extend(value)
+            items_list.extend(value)
+    return items_list
+    
+def map_item_ids(data_4_3_8, items_list_4_8_6):
+    mapped_item_ids = {}
 
     for item_id_4_3_8, item_name_4_3_8 in data_4_3_8.items():
         for item_4_8_6 in items_list_4_8_6:
@@ -71,13 +74,37 @@ def add_automatically_with_manual_mapped_item_ids(mapped_item_ids, manual_mapped
         json.dump(total_mapped_item_ids, f, indent=4)
     print(f"Total mapped item ids: {len(total_mapped_item_ids)}, written to {total_mapped_item_ids_json}")
 
+    return total_mapped_item_ids
+
 def get_manual_mapped_item_ids(file):
     with open(file, "r") as f:
         return json.load(f)
 
+def create_sql_config_list(total_mapped_item_ids, items_list_4_8_6):
+    sql_config_list = []
+    for key, value in total_mapped_item_ids.items():
+        item_4_8_6 = next((item for item in items_list_4_8_6 if item["ID"] == value), None)
+        if item_4_8_6:
+            sql_config = {
+                "item_id": int(key),
+                "name": item_4_8_6["Name"],
+                "description": item_4_8_6["Description"],
+                "type_id": item_4_8_6["ItemType"],
+                "class_id": item_4_8_6["ItemClass"]
+            }
+            sql_config_list.append(sql_config)
+    if not len(sql_config_list) == len(total_mapped_item_ids):
+        raise ValueError("Mismatch in SQL config list length and total mapped item ids length")
+    with open(sql_config_list_json, "w") as f:
+        json.dump(sql_config_list, f, indent=4)
+    print(f"SQL config list with {len(sql_config_list)} items written to {sql_config_list_json}")
+    return sql_config_list
+
 if __name__ == "__main__":
     data_4_3_8 = convert_input_text_to_json(file_4_3_8_txt, file_4_3_8_json)
     data_4_8_6 = get_4_8_6_data(file_4_8_6_json)
-    mapped_item_ids, missed_item_ids = map_item_ids(data_4_3_8, data_4_8_6)
+    items_list_4_8_6 = get_items_list_4_8_6(data_4_8_6)
+    mapped_item_ids, missed_item_ids = map_item_ids(data_4_3_8, items_list_4_8_6)
     manual_mapped_item_ids = get_manual_mapped_item_ids(manual_item_mappings_json)
-    add_automatically_with_manual_mapped_item_ids(mapped_item_ids, manual_mapped_item_ids)
+    total_mapped_item_ids = add_automatically_with_manual_mapped_item_ids(mapped_item_ids, manual_mapped_item_ids)
+    create_sql_config_list(total_mapped_item_ids, items_list_4_8_6)
